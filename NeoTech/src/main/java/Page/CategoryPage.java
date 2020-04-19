6f2113542d34c44b6b53c9f57087cafe9fc096b2
Mahantesh155 <63946033+Mahantesh155@users.noreply.github.com>
2020-04-19 15:31:35 +0530

    Update CategoryPage.java

--- NeoTech/src/main/java/Page/CategoryPage.java
+++ NeoTech/src/main/java/Page/CategoryPage.java
@@ -1,14 +1,12 @@
 package Page;
 
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import NewTech.NeoTech.BaseTest;
 
 public class CategoryPage extends BaseTest{
